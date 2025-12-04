46b1f2f9ab6c340c55b510f77766279498d2c1d6
Oliverio D <oliveriodbs@gmail.com>
2025-12-03 23:49:11 -0600

    pokedex added

--- Sentinel/index.js
+++ Sentinel/index.js
@@ -2,7 +2,12 @@ require("dotenv").config();
 const PREFIX = process.env.PREFIX || "!";
 console.log("Active PREFIX:: [" + PREFIX + "]");
 const TOKEN = process.env.DISCORD_TOKEN;
-const { Client, GatewayIntentBits, Events } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  Events,
+  EmbedBuilder,
+} = require("discord.js");
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -66,6 +71,51 @@ client.on(Events.MessageCreate, async (message) => {
 
         const randomIndex = Math.floor(Math.random() * data.data.length);
         message.reply(data.data[randomIndex].url);
+      } catch (error) {
+        console.log(error);
+        message.reply("An error occurred");
+      }
+      break;
+    case "pokedex":
+      if (args.length === 0) {
+        message.reply("Please provide a Pokemon name");
+        break;
+      }
+      const pokeName = args[0].toLowerCase();
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokeName}`
+        );
+        if (!response.ok) {
+          message.reply("Pokemon not found");
+          break;
+        }
+        const data = await response.json();
+        const name = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+        const id = data.id;
+        const type = data.types.map((t) => t.type.name).join(", ");
+        const hp = data.stats[0].base_stat;
+        const atk = data.stats[1].base_stat;
+        const def = data.stats[2].base_stat;
+
+        const sprite = data.sprites.front_default;
+        const sprite_shiny = data.sprites.front_shiny;
+        const spriteHD =
+          data.sprites.other["official-artwork"].front_default ||
+          data.sprites.front_default;
+        const embed = new EmbedBuilder()
+          .setColor(0xff0000)
+          .setTitle(`${name} #${id}`)
+          .setDescription(`**Types:** ${type}`)
+          .addFields(
+            { name: "HP", value: `${hp}`, inline: true },
+            { name: "Attack", value: `${atk}`, inline: true },
+            { name: "Defense", value: `${def}`, inline: true }
+          )
+          .setImage(spriteHD)
+          .setTimestamp();
+
+        message.reply({ embeds: [embed] });
       } catch (error) {
         console.log(error);
         message.reply("An error occurred");
